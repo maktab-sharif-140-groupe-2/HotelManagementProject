ae115a7538895b670cac2da71eeef95a031bc20b
Mahdi <Mahdi@DESKTOP-ATMOOIR>
2026-06-01 01:01:07 -0700

    CompletHoteUpdateMethod

--- HotelManagementProject.Repository/Repositories/HotelRepository.cs
+++ HotelManagementProject.Repository/Repositories/HotelRepository.cs
@@ -1,4 +1,5 @@
 ﻿using HotelManagementProject.DataAccess.AppDbContextFile;
+using HotelManagementProject.Domain.Dtos;
 using HotelManagementProject.Domain.Entites;
 using HotelManagementProject.Domain.Intefacies;
 using Microsoft.EntityFrameworkCore;
@@ -9,13 +10,13 @@ namespace HotelManagementProject.Repository.Repositories;
 public class HotelRepository : IHotelRepository
 {
     private readonly AppDbContext _context;
-    public async Task<bool> AddHotel(Hotel Hotel)
+    public async Task<bool> AddHotelAsync(Hotel Hotel)
     {
        var add= await _context.Hotels.AddAsync(Hotel);
       return await _context.SaveChangesAsync()>0;
     }
 
-    public async Task<bool> Delete(Guid id)
+    public async Task<bool> DeleteAsync(Guid id)
     {
         var hotel =await GetById(id);
          _context.Hotels.Remove(hotel);
@@ -23,23 +24,30 @@ public class HotelRepository : IHotelRepository
         return await _context.SaveChangesAsync() > 0;
     }
 
-    public async Task<Hotel> GetById(Guid id)
+    public async Task<Hotel> GetByIdAsync(Guid id)
     {
         return await _context.Hotels.FirstOrDefaultAsync(h => h.Id == id);
     }
 
-    public async Task<Hotel> GetHotel(Guid id)
+    public async Task<Hotel> GetHotelAsync(Guid id)
     {
         return await _context.Hotels.FirstOrDefaultAsync(x=>x.Id==id);
     }
 
-    public async Task<List<Hotel>> GetHotels()
+    public async Task<List<Hotel>> GetHotelsAsync()
     {
         return await _context.Hotels.Where(x=>!x.IsDeleted).ToListAsync();
     }
 
-    public Task<bool> UpdateHotel(Guid id, Hotel Hotel)
+    public async Task<bool> UpdateHotelAsync(Guid id, HotelUpdateDto Hotel)
     {
-        _context.Hotels
+        var hotel = await GetByIdAsync(id);
+        if (hotel == null)
+            return false;
+        hotel.City= Hotel.City;
+        hotel.Name=Hotel.Name;
+        _context.Hotels.Update(hotel);
+        return await _context.SaveChangesAsync() > 0;
+
     }
 }
