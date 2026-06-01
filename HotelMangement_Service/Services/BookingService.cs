470530c934253290677dc51c3a2f5d41c0d0fe57
farjad <11@DESKTOP-A87MHH8>
2026-06-01 23:03:48 +0330

    change

--- HotelMangement_Service/Services/BookingService.cs
+++ HotelMangement_Service/Services/BookingService.cs
@@ -15,11 +15,7 @@ namespace HotelMangement_Service.BookingServices
         private IRoomRepository _roomRepository;
         public async Task<bool> CreateBooking(int roomId,DateTime enteryDate ,int daysofStay)
         {
-            if (enterDate > DateTime.Now)
-            {
-                throw new ArgumentException( "invalid Enterydate ");
-            }
-                var room=await _roomRepository.GetByIdAsync(roomId);
+           var room=await _roomRepository.GetByIdAsync(roomId);
             if (room == null)
             {
                 throw new ArgumentNullException("RoomIdCant be null");
@@ -28,7 +24,7 @@ namespace HotelMangement_Service.BookingServices
                 throw new ArgumentException("cant be zero or more than 100");
             if (daysofStay <= 100)
                 throw new ArgumentException("cant be zero or more than 100");
-            var booking = new Booking(roomId, enterDate, DateTime.Now.AddDays(daysofStay));
+            var booking = new Booking(roomId,DateTime.Now,DateTime.Now.AddDays(daysofStay));
 
            var result= await _bookingRepository.AddAsync(booking);
             return result;
