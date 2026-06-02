0caad13221ffeb301853b11cc0af817e9f40ebdf
Mahdi <Mahdi@DESKTOP-ATMOOIR>
2026-06-01 22:00:27 -0700

    Completed Guest Validate and add National Id to properties

--- HotelManagementProject.Domain/Entites/Guest.cs
+++ HotelManagementProject.Domain/Entites/Guest.cs
@@ -4,11 +4,34 @@ namespace HotelManagementProject.Domain.Entites;
 
 public class Guest : BaseEntity
 {
+    public Guest(string fullName, string nationalId)
+    {
+        FullName = fullName;
+        NationalId = nationalId;
+        Validation();
+        ValidateNationalCode(NationalId);
+    }
+
     public string FullName { get; set; }
+    public string NationalId { get; set; }
     public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 
     protected override void Validation()
     {
-        throw new NotImplementedException();
+        if(string.IsNullOrEmpty(FullName))
+            throw new ArgumentNullException("FullName Can't be null",nameof(FullName));
     }
+    private void ValidateNationalCode(string nationalCode)
+    {
+        if (string.IsNullOrWhiteSpace(nationalCode))
+            throw new ArgumentNullException("your NationalCode cannot be null or empty");
+
+        if (nationalCode.Length != 10)
+            throw new InvalidOperationException("the national code length is less or higher than 10 characters");
+
+        if (!nationalCode.All(char.IsDigit))
+            throw new InvalidOperationException("for thee national code all characters must be digit");
+    }
+
+
 }
