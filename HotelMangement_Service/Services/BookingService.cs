08f0ca11983b22be5df1f0971bc2cca3bbcce339
Mahdi <Mahdi@DESKTOP-ATMOOIR>
2026-06-02 04:31:42 -0700

    Add checking about conflictBooking

--- HotelMangement_Service/Services/BookingService.cs
+++ HotelMangement_Service/Services/BookingService.cs
@@ -31,16 +31,19 @@ namespace HotelMangement_Service.Services
                 throw new ArgumentException("days of stay can't be zero");
             if (daysofStay > 100)
                 throw new ArgumentException("days of stay can't be more than 100");
+            await CheckForConflictBooking(roomId,enteryDate,daysofStay);
             var booking = new Booking(roomId,enteryDate, enteryDate.AddDays(daysofStay));
             return await _bookingRepository.AddAsync(booking);
         }
 
-        private async Task<bool> CheckForConflictBooking(Guid roomId, DateTime enteryDate, int daysofStay)
+        private async Task<bool> CheckForConflictBooking(Guid roomId, DateOnly enteryDate, int daysofStay)
         {
            var room= await _roomRepository.GetByIdAsync(roomId);
-            if(room.Bookings.Any(x=> x.CheckIn==)) {
-
-
+            if (room.Bookings.Any(x => x.CheckIn <= enteryDate && x.CheckOut > enteryDate))
+                throw new InvalidDataException("this time room is reserved");
+            if(room.Bookings.Any(x=> x.CheckIn< enteryDate.AddDays(daysofStay)))
+                throw new InvalidDataException("day ofStay can't be Apply(Room reserved)");
+            return true;
         }
 
 
